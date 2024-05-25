931af690953f6cc6a60b876bf7ddf4cf0f87d00e
Kai Steltner <steltner.kai@gmail.com>
2024-05-25 23:05:02 +0200

    Add reset button

--- javascript.js
+++ javascript.js
@@ -1,41 +1,52 @@
 let nSquareBase = 16;
 let mouseDown = false;
-const grid = document.querySelector("#grid");
+const grid = document.querySelector('#grid');
+const btnReset = document.querySelector('#btn-reset');
 
 // Function definitions
 function addSquares() {
-    // Set up grid of empty squares
-    const baselength = grid.clientWidth / nSquareBase;
-    for (let idxRow = 0; idxRow < nSquareBase; idxRow++) {
-        const row = document.createElement("div");
-        row.classList.add('grid-row');
-        for (let idxCol = 0; idxCol < nSquareBase; idxCol++) {
-            const square = document.createElement("div");
-            square.classList.add('square');
-            square.style.minWidth = `${baselength}px`;
-            square.style.minHeight = `${baselength}px`;
-            row.appendChild(square);
-        }
-        grid.appendChild(row);
+  // Set up grid of empty squares
+  const baselength = grid.clientWidth / nSquareBase;
+  for (let idxRow = 0; idxRow < nSquareBase; idxRow++) {
+    const row = document.createElement('div');
+    row.classList.add('grid-row');
+    for (let idxCol = 0; idxCol < nSquareBase; idxCol++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      square.style.minWidth = `${baselength}px`;
+      square.style.minHeight = `${baselength}px`;
+      row.appendChild(square);
     }
+    grid.appendChild(row);
+  }
 }
 
 function colorSquare(e) {
-    e.target.style.backgroundColor = "black";
+  e.target.style.backgroundColor = 'black';
 }
 
 // Callbacks
-document.body.onmousedown = function() { 
+document.body.onmousedown = function () {
   mouseDown = true;
-}
-document.body.onmouseup = function() {
+};
+document.body.onmouseup = function () {
   mouseDown = false;
-}
+};
 
 grid.onmousedown = (e) => {
-    colorSquare(e);
-    grid.onmousemove = (e) => colorSquare(e)};
-grid.onmouseup = () => {grid.onmousemove = null};
+  colorSquare(e);
+  grid.onmousemove = (e) => colorSquare(e);
+};
+grid.onmouseup = () => {
+  grid.onmousemove = null;
+};
+
+btnReset.onclick = () => {
+  squares = document.querySelectorAll('.square');
+  squares.forEach((square) => {
+    square.style.backgroundColor = 'white';
+  });
+};
 
 // Startup
 addSquares();
